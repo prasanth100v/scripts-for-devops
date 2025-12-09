92bbc76986c61a8bcfe6d844a1b3d79d70780253
prasanth <123481513+prasanth100v@users.noreply.github.com>
2025-12-09 14:58:26 +0530

    Update and rename terrofom install for RedHat.sh to terrofom-RedHat&centOS.sh

---
+++ terrofom-RedHat&centOS.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+sudo yum -y install terraform
+
+echo "=== Done ==="
